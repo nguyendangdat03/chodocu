6a8ecb94ab128b455ab969dade854c967790c312
nguyendangdat03 <nguyendat7995@gmail.com>
2025-01-14 17:29:35 +0700

    feat

--- src/public/public.module.ts
+++ src/public/public.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PublicController } from './public.controller';
 import { ProductModule } from '../product/product.module';
-import { CategoryModule } from '../category/category.module'; // Import CategoryModule
+import { CategoryModule } from '../category/category.module';
+import { BrandModule } from 'src/brand/brand.module'; // Import CategoryModule
 @Module({
-  imports: [ProductModule, CategoryModule], // Import CategoryModule
+  imports: [ProductModule, CategoryModule, BrandModule], // Import CategoryModule
   controllers: [PublicController],
 })
 export class PublicModule {}
